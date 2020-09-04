fbde9fd3fcb11a7558628113cdd9f69a5f576d84
MikeChuv <mchuvorkin@gmail.com>
2020-09-04 23:25:28 +0300

    Readme and scripts

---
+++ cmds.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+function cpplab() {
+	labname=$1
+	mkdir $labname
+	cd $labname
+	cp -i /home/mike/Docs/prog_c_and_cpp/start.sh /home/mike/Docs/prog_c_and_cpp/$labname/
+	touch source.cpp
+	touch README.md
+	echo Created files for $labname
+}
+
