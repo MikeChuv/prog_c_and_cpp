6ceefa813aa6217ae2e4ae07620c7b0b229e9050
MikeChuv <mchuvorkin@gmail.com>
2020-10-27 17:17:45 +0300

    Lab04 to array done

--- Lab04/source.cpp
+++ Lab04/source.cpp
@@ -4,15 +4,7 @@ volatile int capacity = 10;
 
 int compare(char* mystring, char* sub1, char* sub2){
 	int result = 0;
-	if (*sub1 == *mystring){
-		//printf("%c", *mystring);
-		result = compare(mystring + 1, sub1 + 1, sub2);
-	}
-	else if (*sub1 == '\0') {
-		//printf("%s%c", sub2, *mystring);
-		// передвинуть хвост на s2s позиций и вставить нужные символы
-	}
-	//else //printf("%c", *mystring);
+	if (*sub1 == *mystring) result = compare(mystring + 1, sub1 + 1, sub2);
 	return 1 + result;
 }
 
@@ -34,7 +26,6 @@ char* readfn(FILE* input, int mode, int &chars){
 	}
 	while(  (mode && !feof(input))  ||  (!mode && (ch != '\n'))  );
 	chars = i - 1;
-	printf("Size = %d\n", chars);
 	return mystring;
 }
 
@@ -43,17 +34,19 @@ void compare_loop(char* mystring, char* subs1, char* subs2, int& mss, int s1s, i
 	while(mystring[i] != '\0') {
 		shift = compare(mystring + i, subs1, subs2); 
 		if ( (shift - 1) == s1s){
-			printf("\nInserting: s2s = %d\n", s2s);
 			if (size - mss >= s2s){
-				// вставить элементы	
 				for(int j = mss + 1; j >= i + s1s; j--) mystring[j + s2s] = mystring[j]; 
 				mss += s2s;
 				for(int j = 0; j < s2s; j++) mystring[i + s1s + j] = subs2[j];
 			}
 			else{
-				// расширяем и вставляем
+				mystring = resize(mystring);
+				size = capacity;
+				for(int j = mss + 1; j >= i + s1s; j--) mystring[j + s2s] = mystring[j]; 
+				mss += s2s;
+				for(int j = 0; j < s2s; j++) mystring[i + s1s + j] = subs2[j];
 			}
-			printf("\nThey are equal\n free: %d\n", size - mss);
+			printf("Free: %d\n", size - mss);
 		}
 		i += shift; 
 	}
@@ -67,14 +60,13 @@ int main(int argc, char* argv[]){
 	char *mystring, *subs1, *subs2;
 	int mss, s1s, s2s;
 	mystring = readfn(input_file, 1, mss);
-	int kek = capacity;
-	printf("Kek = %d", kek);
+	int reserved = capacity;
 	printf("Subs 1 = ");
 	subs1 = readfn(input_file, 0, s1s);
 	printf("Subs 2 = ");
 	subs2 = readfn(input_file, 0, s2s);
 	fseek(input_file, 0, SEEK_SET);
-	compare_loop(mystring, subs1, subs2, mss, s1s, s2s, kek);
+	compare_loop(mystring, subs1, subs2, mss, s1s, s2s, reserved);
 	fclose(input_file);
 	delete [] mystring;
 	delete [] subs1;
